4445a59ef89244740e05ad1e8efc3150c305f132
John Wells <molekewl2017@gmail.com>
2019-12-25 22:33:28 -0800

    callback process sum solved using reduce

--- index.js
+++ index.js
@@ -89,8 +89,11 @@ function processLastItem(arrayList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
+function processSum(numberList, callback) {
   /* CODE HERE */
+  return numReduce = callback(numberList.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0));
 }
 
 /**
