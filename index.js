07fa03472c8a1da1e07184927f6a244014abbff8
John Wells <molekewl2017@gmail.com>
2019-12-28 18:57:34 -0800

    process contains

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function processProduct(num1, num2, callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
+function processContains(item, list, callback) {
   /* CODE HERE */
 }
 
